ef8ce2443b9c536ee4911d0fab0e9a07b51169c1
vinicius <viniciusxyz@protonmail.com>
2020-03-31 23:07:45 -0300

    Mod4 + q kill client

--- config.def.h
+++ config.def.h
@@ -92,7 +92,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,            {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,      {0} },
     { MODKEY,                       XK_f,      togglefullscr,   {0} },
-
+    { MODKEY,                       XK_q,      killclient,      {0} },
+    { MODKEY|ShiftMask,             XK_q,      killclient,      {0} },
   /*{ MODKEY,                       XK_t,      setlayout,       {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,       {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,       {.v = &layouts[2]} }, */
